c24ae79ccf33c03ee06aed4a9e8d7bc2c60656ff
Vitor Mendes <34409244+vitormndc@users.noreply.github.com>
2019-08-24 19:08:36 -0300

    Add files via upload

---
+++ list_cleaner.py
@@ -0,0 +1,23 @@
+'''
+The listc function needs two lists to work, the first one is the list you wanna clean,
+and the second is the characters you want to remove.
+'''
+cleaned_list = []
+
+def list_cleaner(my_list, remove):
+
+    for iten in my_list:
+        has_symbol = False
+
+        for symbol in remove:
+            symbol = str(symbol)
+
+            if symbol in iten:
+                iten = iten.replace(symbol, '')
+            else:
+                pass
+
+        cleaned_list.append(iten)
+
+    return cleaned_list
+
