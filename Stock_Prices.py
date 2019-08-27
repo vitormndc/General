7109115de3e0678553f3f0b258de39155392795e
Vitor Mendes <34409244+vitormndc@users.noreply.github.com>
2019-08-27 13:16:40 -0300

    Update Stock_Prices.py

--- Stock_Prices.py
+++ Stock_Prices.py
@@ -9,6 +9,21 @@ up = '▲'
 down = '▼'
 
 
+def string_cleaner(my_stg, remove):
+    # this function could be imported as a module that i made, it is here just to make easier if someone wants just
+    # this script
+
+    for symbol in remove:
+        symbol = str(symbol)
+
+        if symbol in my_stg:
+            my_stg = my_stg.replace(symbol, '')
+        else:
+            pass
+
+    return my_stg
+
+
 def my_url(stock):  # pass the stock name to the NASDAQ realtime site
     url = f'https://www.nasdaq.com/symbol/{stock}/real-time'
 
@@ -22,35 +37,49 @@ def up_or_down(var):  # identifies if the stock has gone up or down
         return down
 
 
-def formatter(c_info):  # abbreviation of Company_information that comes in the response list.
-    c_str = f'{c_info[0]}' + ' ' * (50 - len(c_info[0])) + f'{c_info[1]}' + ' '*(15 - len(c_info[1])) + f'{c_info[2]}'\
-               + ' ' * (20 - len(c_info[2])) + f'{c_info[3]}' + ' ' * (3 - len(c_info[3])) + f'{c_info[4]} %'
+def formatter(c_info, spaces=None):  # c_info is the abbreviation of Company_information that is given
+    c_str = ''                       # spaces is the number of spaces between columns in the final print
+    if spaces is None:
+        spaces = [55, 15, 20, 3, 0]  # default values for columns spacing
+    for (j, k) in zip(c_info, spaces):
+        c_str += str(j)+' '*(k-len(j))
     return c_str
 
 
-stocks = str(input('Which stocks would you like to search? (e.g., TSLA, AAPL, FB):\n')).upper().replace(',', ' ').split()
+while True:
 
+    input_str = 'Which stocks would you like to search? (e.g., TSLA, AAPL, FB):\n'
+    stocks = str(input(input_str)).upper().replace(',', ' ')
+    stocks = stocks.split()
+    stocks = list(dict.fromkeys(stocks))
 
-print("\n\n")
-for i in tqdm(stocks):
-    try:
-        page = requests.get(my_url(i), headers=header)
-        my_html = html.fromstring(page.content)
-        company_name = str(my_html.xpath('//*[@id="qwidget_pageheader"]/h1/text()')[0]).replace("Common Stock", '') \
-            .replace('Real Time Stock Quotes', '')
-        price = my_html.xpath('//*[@id="qwidget_lastsale"]/text()')[0]
-        variation = my_html.xpath('//*[@id="qwidget_netchange"]/text()')[0]
-        variation_direction = my_html.xpath('//*[@id="qwidget_netchange"]')[0].items()
-        response.append([company_name[0:45], i[0:9], price, up_or_down(variation_direction), variation])
-    except:
-        failed_search.append(i)
+    for i in tqdm(stocks):
+        try:
+            page = requests.get(my_url(i), headers=header)
+            my_html = html.fromstring(page.content)
+            company_name = str(my_html.xpath('//*[@id="qwidget_pageheader"]/h1/text()')[0])
+            company_name_replaces = ['Common Stock', 'Real Time Stock Quotes', 'Class A']
+            company_name = string_cleaner(company_name, company_name_replaces)
+            price = my_html.xpath('//*[@id="qwidget_lastsale"]/text()')[0]
+            variation = my_html.xpath('//*[@id="qwidget_netchange"]/text()')[0]
+            variation_direction = my_html.xpath('//*[@id="qwidget_netchange"]')[0].items()
+            response.append([company_name[0:45], i[0:9], price, up_or_down(variation_direction), variation])
+        except:
+            failed_search.append(i)
 
-if len(response) > 0:
-    list_header = ['Company name', 'Symbol', 'Stock Price', 'Today fluctuation', '']
-    print('\n' * 5 + formatter(list_header).replace('%', '') + '\n(Max, 50 characters) \n')
+    if len(response) > 0:
 
-    for i in response:
-        print(formatter(i))
+        list_header = ['Company name', 'Symbol', 'Stock Price', 'Today fluctuation', '']
+        print('\n' * 5 + formatter(list_header).replace('%', '') + '\n(Max, 50 characters) \n')
 
-if failed_search:
-    print(f"\n\n\nCouldn't find these companies: {str(failed_search)}")
+        for i in response:
+            print(formatter(i))
+
+    if failed_search:
+
+        print(f"\n\n\nCouldn't find these companies: {str(failed_search)}\n")
+
+    goagain = str(input('\n\nWould you like to make another search? (y or n): \n'))
+    if goagain is not 'y':
+        break
+
