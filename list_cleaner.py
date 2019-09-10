f6ca867140f587477417829225d01fcc4c574838
Vitor Mendes <34409244+vitormndc@users.noreply.github.com>
2019-09-10 15:21:04 -0300

    Update list_cleaner.py

--- list_cleaner.py
+++ list_cleaner.py
@@ -1,7 +1,3 @@
-'''
-The lc function needs two lists to work, the first one is the list you wanna clean,
-and the second is the characters you want to remove from itens in the first list.
-'''
 cleaned_list = []
 
 def lc(my_list, remove):
