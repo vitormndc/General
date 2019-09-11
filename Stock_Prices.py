ff0ba951fea9f3f19039f96bc85346e5e5a010d9
Vitor Mendes <34409244+vitormndc@users.noreply.github.com>
2019-09-11 17:01:13 -0300

    Added save functionality
    TODO. add search from save

--- Stock_Prices.py
+++ Stock_Prices.py
@@ -1,10 +1,10 @@
 import requests
+import json
 from lxml import html
 from tqdm import tqdm
 
 response = []
 failed_search = []
-header = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 up = '▲'
 down = '▼'
 
@@ -22,14 +22,14 @@ def string_cleaner(my_stg, remove):
     return my_stg
 
 
-def my_url(stock):  # pass the stock name to the NASDAQ realtime site
-    url = f'https://www.nasdaq.com/symbol/{stock}/real-time'
+def my_url(stock_name):
+    url = f'https://www.nasdaq.com/symbol/{stock_name}/real-time'
 
     return url
 
 
-def up_or_down(var):  # identifies if the stock has gone up or down
-    if 'green' in str(var).lower():
+def up_or_down(html_string):  # identifies if the stock has gone up or down
+    if 'green' in str(html_string).lower():
         return up
     else:
         return down
@@ -44,26 +44,45 @@ def formatter(c_info, spaces=None):  # c_info is the abbreviation of Company_inf
     return c_str
 
 
+def saver(save_name):
+    stock_list = list(set(stocks) - set(failed_search))
+    print(stock_list)
+    try:
+        with open('stock_saves.json', 'r+') as json_file:
+            data = json.load(json_file)
+            json_file.seek(0)
+            data[save_name] = stock_list
+            json.dump(data, json_file, indent=4)
+    except:
+        with open('stock_saves.json', 'w') as json_file:
+            data = {save_name: stock_list}
+            json.dump(data, json_file, indent=4)
+
+
+def opener(save_name):
+    pass
+
+
 while True:
     input_str = 'Which stocks would you like to search? (e.g., TSLA, AAPL, FB):\n'
     stocks = str(input(input_str)).upper().replace(',', ' ')
     stocks = stocks.split()
-    stocks = list(dict.fromkeys(stocks))
+    stocks = list(dict.fromkeys(stocks))   # remove repeated names
 
     print('\n\n\n')
-    for i in tqdm(stocks):
+    for stock in tqdm(stocks):
         try:
-            page = requests.get(my_url(i), headers=header)
+            page = requests.get(my_url(stock))
             my_html = html.fromstring(page.content)
             company_name = str(my_html.xpath('//*[@id="qwidget_pageheader"]/h1/text()')[0])
             company_name_replaces = ['Common Stock', 'Real Time Stock Quotes', 'Class A']
-            company_name = string_cleaner(company_name, company_name_replaces)
+            company_name = string_cleaner(company_name, company_name_replaces)    # sanitize the company name
             price = my_html.xpath('//*[@id="qwidget_lastsale"]/text()')[0]
             variation = my_html.xpath('//*[@id="qwidget_netchange"]/text()')[0]
             variation_direction = my_html.xpath('//*[@id="qwidget_netchange"]')[0].items()
-            response.append([company_name[0:45], i[0:9], price, up_or_down(variation_direction), variation + ' %'])
+            response.append([company_name[0:45], stock[0:9], price, up_or_down(variation_direction), variation + ' %'])
         except:
-            failed_search.append(i)
+            failed_search.append(stock)
 
     if response:
 
@@ -76,12 +95,16 @@ while True:
         response.clear()
 
     if failed_search:
-
         print(f"\nCouldn't find these companies: {str(failed_search)}\n")
         failed_search.clear()
 
-    repeat = str(input('\n\nWould you like to make another search? (y or n): '))
-    print('\n\n')
+    save = str(input('\nWould you like to save this search? (y or n): '))
+    if save is 'y':
+        save_as = str(input('\nHow would you like to name the save?: '))
+        saver(save_as)
+
+    repeat = str(input('\nWould you like to make another search? (y or n): '))
     if repeat is not 'y':
         break
 
+    print('\n'*10)
