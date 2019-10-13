04690912f8448c572c6573aa130fecc50383e440
Vitor Mendes <34409244+vitormndc@users.noreply.github.com>
2019-10-13 20:46:14 -0300

    Ignore spaces

--- Dumber_a_sentence.py
+++ Dumber_a_sentence.py
@@ -2,11 +2,18 @@ def dumber(string):
 
     new_string = ''
     for c, i in enumerate(string):
+        previous_letter = -1
+
         if c == 0:
             new_string = string[c].upper()
+
         else:
-            if new_string[c-1].isupper():
+            while string[c+previous_letter] == ' ':  # if the last character is a space it will look for the last letter
+                previous_letter += -1
+
+            if new_string[c+previous_letter].isupper():
                 new_string += string[c].lower()
             else:
                 new_string += string[c].upper()
+
     return new_string
